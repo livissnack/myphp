7c9a7852c71f75caf19931b33e81cfe93594882d
brucesnack <brucesnack@outlook.com>
2023-08-24 16:34:53 +0800

    tset

--- api/jsptv.php
+++ api/jsptv.php
@@ -1,33 +1,52 @@
 <?php
+date_default_timezone_set("Asia/Shanghai");
 $id = isset($_GET['id'])?$_GET['id']:'jxws';
 $n = [
-    'jxws' => 'tv_jxtv1.m3u8',//江西卫视
-    'jxds' => 'tv_jxtv2.m3u8',//江西都市
-    'jxjs' => 'tv_jxtv3_hd.m3u8',//江西经视
-    'jxys' => 'tv_jxtv4.m3u8',//江西影视
-    'jxgg' => 'tv_jxtv5.m3u8',//江西公共
-    'jxse' => 'tv_jxtv6.m3u8',//江西少儿
-    'jxxw' => 'tv_jxtv7.m3u8',//江西新闻
-    'jxyd' => 'tv_jxtv8.m3u8',//江西移动
-    'fsgw' => 'tv_fsgw.m3u8',//江西风尚购物
-    ];
-
-$timestamp = intval(microtime(true)*1000);
-$params = "type=android&t={$timestamp}&time={$timestamp}&token=md5('com.sobey.cloud.view.jiangxiandroidjxntv'.$timestamp)&device_id=48MXQ6GIMM8UYXKJ734O2NMWEBKBWH49LB4EE9R0&app_version=5.07.09&siteid=10001";
-$authUrl = "https://app.jxgdw.com/api/media/auth?".$params;
-$d = file_get_contents($authUrl);
-$json = json_decode($d);
-$live = "https://jsp-tv-live.jxtvcn.com.cn/live-jsp/{$n[$id]}";
-$tail = http_build_query($json);
-$m3u8 = trim("$live?$tail");
-
-$ch = curl_init($m3u8);
+    'jxws' => 'tv_jxtv1.m3u8',
+    'jxds' => 'tv_jxtv2.m3u8',
+    'jxjs' => 'tv_jxtv3.m3u8',
+    'jxjshd' => 'tv_jxtv3_hd.m3u8',
+    'jxys' => 'tv_jxtv4.m3u8',
+    'jxgg' => 'tv_jxtv5.m3u8',
+    'jxse' => 'tv_jxtv6.m3u8',
+    'jxxw' => 'tv_jxtv7.m3u8',
+    'jxyd' => 'tv_jxtv8.m3u8',
+    'fsgw' => 'tv_fsgw.m3u8',
+    'jxtc' => 'tv_taoci.m3u8',
+];
+$timestamp = time();
+function etag() {
+    $e = "ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678oOLl9gqVvUuI1";
+    $a = "";
+    for ($i = 0; $i < 8; $i++) {
+        $a .= $e[rand(0,  strlen($e)-1)];
+    }
+    return $a;
+}
+$etag = etag();
+$auth = md5('1609229748'.$n[$id].$etag .'233face@12&^a');
+$token_url = "https://app.jxntv.cn/Qiniu/liveauth/getPCAuth.php";
+$info = 't=1609229748&stream='.$n[$id].'&uuid=0f408901f01b&';
+$headers = array(
+    'etag:'.$etag,
+    'Authorization:'.$auth,
+    "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
+    "origin: http://www.jxntv.cn",
+    "Referer: http://www.jxntv.cn/",
+    "Content-Type: application/x-www-form-urlencoded;charset=UTF-8",
+);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $token_url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-$play = curl_exec($ch);       
+curl_setopt($ch, CURLOPT_POST, TRUE);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $info);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+$response = curl_exec($ch);
 curl_close($ch);
-$burl = 'https://jsp-tv-live.jxtvcn.com.cn/live-jsp/';
-header("Content-Type: application/vnd.apple.mpegURL");
-print_r(preg_replace("/(.*?.ts)/i",$burl."$1",trim($play)));
-?>
+$responseArray = json_decode($response, true);
+$t = $responseArray['t'];
+$token = $responseArray['token'];
+$playurl = "https://yun-live.jxtvcn.com.cn/live-jxtv/".$n[$id]."?source=pc&t=".$t."&token=".$token;
+var_dump($playurl);die();
+header('Location:'.$playurl);
