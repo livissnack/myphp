aeb54616f168391b0b695b4bc86139659fff8c73
brucesnack <brucesnack@outlook.com>
2024-02-06 09:42:16 +0800

    update

--- api/nbtv.php
+++ api/nbtv.php
@@ -15,13 +15,12 @@
     $headers = [
         'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML,like Gecko) Chrome/99.0.4844.84 Safari/537.36 HBPC/12.1.3.3',
         'Host: em.chinamcloud.com',
-        'Origin: em.chinamcloud.com',
+        'Content-Type: application/json',
         'Referer: em.chinamcloud.com',
     ];
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($ch,CURLOPT_POST, 1);
     curl_setopt($ch,CURLOPT_POSTFIELDS, json_encode($params));
     curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
